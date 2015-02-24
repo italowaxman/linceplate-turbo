80768dede5682c6a2bbbcb62f47e3b3d84a90835
Ítalo Waxman <italowaxman@gmail.com>
2015-02-24 10:57:07 -0300

    Estrutura inicial do boilerplate.

---
+++ _footer.php
@@ -0,0 +1,18 @@
+<footer>
+
+</footer><!-- fim footer-->
+
+<!-- Scripts minificados e concatenados -->
+<script src="assets/js/build/all.min.js"></script>
+
+<!-- Google Analytics: Altere UA-XXXXX-X pelo ID do seu site. -->
+<script>
+	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	g.src='//www.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+
+<!-- Habilita o Livereaload do Grunt / Comentar ao publicar-->
+<script src="//localhost:35729/livereload.js"></script>
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<? include '_head.php'; ?>
+<body>
+    <? include '_chromeframe.php'; ?>
+    <div class="wrapper">
+        <? include '_header.php'; ?>
+
+        <!-- ***************************************** -->
+
+        <!-- Adicione o conteúdo do seu projeto aqui. -->
+
+        <!-- ***************************************** -->
+
+        <? include '_footer.php'; ?>
+    </div>
+</body>
+</html>
